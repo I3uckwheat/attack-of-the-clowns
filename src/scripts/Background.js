7e4376beb4f3edbf8f4aa5a6f140bfbe85e7c2a3
Morrow Davidson <morrow.davidson@gmail.com>
2019-04-24 09:05:56 -0700

    Fix parallax

--- src/scripts/Background.js
+++ src/scripts/Background.js
@@ -4,13 +4,11 @@ const background = {
       element: document.getElementById('playableArea'),
       position: 0,
       speed: 5,
-      reset: 520,
     },
     crowd: {
       element: document.getElementById('background'),
       position: 0,
       speed: 4,
-      reset: 520,
     },
   }, 
   right: function() {
@@ -25,7 +23,7 @@ const background = {
   },
   draw: function() {
     this.layers.ring.element.style.backgroundPositionX = this.layers.ring.position + 'px';
-    this.layers.crowd.element.style.backgroundPositionX = this.layers.ring.position + 'px';
+    this.layers.crowd.element.style.backgroundPositionX = this.layers.crowd.position + 'px';
   }
 }
 
